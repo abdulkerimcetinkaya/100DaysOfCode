f65786619cee86e1698a0c7583450cfaec542c73
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-10-01 01:08:27 +0300

    Uploaded Project1

---
+++ ProjectDay1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("which city did you grow up in? : ")
+pet = input("What is the name of a pet? : ")
+print("your band name could be : " + city + " " + pet)
